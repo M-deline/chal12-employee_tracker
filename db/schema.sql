be4c2ac9c5d1f440dd94c1b473fc9b473afee0bf
Madeline K <madkovanda@gmail.com>
2023-09-26 18:24:12 -0600

    update server having schema issue

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ USE employees_db;
 
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    department_name VARCHAR(35) NOT NULL UNIQUE,
-)
+    department_name VARCHAR(35) NOT NULL UNIQUE
+);
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -15,16 +15,16 @@ CREATE TABLE roles (
     departments_id INT NOT NULL,
     FOREIGN KEY (departments_id)
     REFERENCES departments(id)
-) 
+);
 
 CREATE TABLE employees(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(35) NOT NULL, 
     last_name VARCHAR(35) NOT NULL, 
     job_id NOT NULL,
-     manager_id INT,
+    manager_id INT,
     FOREIGN KEY(job_id)
     REFERENCES roles(id),
     FOREIGN KEY (manager_id)
     REFERENCES employees(id)
-)
+);
